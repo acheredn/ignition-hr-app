6542b4f35a119c10147f65ffc5b454097f48df10
Anatoliy Cherednichenko <cherednichenko.anatoly@gmail.com>
2025-10-19 23:49:09 -0400

    added clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Script from "next/script";
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,14 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <Script
-          src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
-          strategy="beforeInteractive"
-        />
-      </head>
-      <body className="antialiased">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <Script
+            src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
+            strategy="beforeInteractive"
+          />
+        </head>
+        <body className="antialiased">{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
